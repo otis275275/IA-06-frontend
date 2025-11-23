c7360f34b3f3b0df9b8d2895ac1fd11f3f326e66
Otis <ledat02072005@gmail.com>
2025-11-23 21:51:58 +0700

    fixed deployment

--- src/service/api.js
+++ src/service/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const API_URL = process.env.VITE_API_URL;
 
 const api = axios.create({
-    baseURL: API_URL,
+    baseURL: `${API_URL}/api`,
     headers: {
         'Content-Type': 'application/json',
     },
