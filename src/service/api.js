6dab6cf9e44725116e8454e11ca60f80a2ab4e49
Otis <ledat02072005@gmail.com>
2025-11-24 09:17:48 +0700

    a

--- src/service/api.js
+++ src/service/api.js
@@ -3,7 +3,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 console.log(API_URL)
 
 const api = axios.create({
-    baseURL: 'https://ia-06-backend.vercel.app',
+    baseURL: 'https://ia-06-backend.vercel.app/api',
     headers: {
         'Content-Type': 'application/json'
     },
